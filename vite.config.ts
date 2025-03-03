7c9ac91445f5943c50e79e3cbba8bd84b7dcc867
otaviopereira10 <otavioleedz@gmail.com>
2025-03-03 02:02:06 -0300

    Inicializando repositório

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/Portifolio-OFC/", // 🔥 EXATAMENTE o nome do repositório GitHub
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  build: {
-    outDir: "dist",
-    assetsDir: "assets",
-    emptyOutDir: true,
   },
 });
